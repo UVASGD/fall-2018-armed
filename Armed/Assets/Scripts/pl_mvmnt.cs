f903d1041c375628d5e67c768fcc3b312f6b8863
Salindar165 <klanax135@gmail.com>
2018-05-28 21:01:43 -0400

    My Spaghet code is now lasagna
    MMmmmmmmm, Lasagna

--- Armed/Assets/Scripts/pl_mvmnt.cs
+++ Armed/Assets/Scripts/pl_mvmnt.cs
@@ -18,31 +18,28 @@ public class pl_mvmnt : MonoBehaviour {
     void Sprinting(ref float speed, ref float curr_stamina, ref float stamina_regen)
         {
         if (Input.GetKey(KeyCode.LeftShift) && curr_stamina > 0) {
-            speed = 0.5f;
+            speed = 15f;
             curr_stamina --;
             stamina_regen = 0;
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            speed = 0.25f;
+            speed = 8f;
             stamina_regen = 1;
         }
         else
         {
-            // speed = 15f;
-            speed = 0.25f;
+            speed = 8f;
         }
     }
 
     //FixedUpdate is independant of frame rate <3
     private void FixedUpdate()
-    {
-        //Movement with Translate 
-
+    {        
         //Updates speed and curr_stamina by checking for sprinting
         Sprinting(ref speed, ref curr_stamina, ref stamina_regen);
 
-        //Update Sprinting
+        //Update Stamina
         if (curr_stamina < 0)
         {
             curr_stamina = 0;
@@ -57,39 +54,10 @@ public class pl_mvmnt : MonoBehaviour {
         }
 
         //Movement of the Player
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            Vector3 position = this.transform.position;
-            position.x = position.x + speed;
-            this.transform.position = position;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Vector3 position = this.transform.position;
-            position.x = position.x - speed;
-            this.transform.position = position;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            Vector3 position = this.transform.position;
-            position.y = position.y + speed;
-            this.transform.position = position;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            Vector3 position = this.transform.position;
-            position.y = position.y - speed;
-            this.transform.position = position;
-        }
-       
-
-        /*
         float HorizontalMove = Input.GetAxis("Horizontal");
         float VerticalMove = Input.GetAxis("Vertical");
-        Vector2 move = new Vector2(HorizontalMove, VerticalMove);
-        transform.Translate(move * speed * Time.deltaTime);
-        */
+        Vector2 move = new Vector2(HorizontalMove*speed, VerticalMove*speed);
+        transform.Translate(move * Time.deltaTime,Space.World);
 
         //Rotation of the Player
         Vector2 playerpos = Camera.main.WorldToViewportPoint(transform.position);   //Define Player Position
@@ -97,52 +65,8 @@ public class pl_mvmnt : MonoBehaviour {
         float angle = AngleBetweenTwoPoints(playerpos, mousepos) + 90;   //Solve for the angle between the player and mouse
         transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));  //Rotate Player
 
-
-
-        //Movement with Rigidbodies and Forces
-
-        /*
-        //Update speed and curr_stamina by checking for sprinting
-        Sprinting(ref speed, ref curr_stamina, ref stamina_regen);
-
-        //Update Stamina
-        if (curr_stamina < 0)
-        {
-            curr_stamina = 0;
-        }
-        else if (curr_stamina > maxstamina)
-        {
-            curr_stamina = maxstamina;
-        }
-        else
-        {
-            curr_stamina = curr_stamina + stamina_regen;
-        }
-
-        //Get Horizontal Movement
-        float HorizontalMove = Input.GetAxis("Horizontal");
-
-        //Get Vertical Movement
-        float VerticalMove = Input.GetAxis("Vertical");
-
-        //Create movement Vector
-        Vector2 move = new Vector2(HorizontalMove, VerticalMove);
-        //Debug.Log(move);
-        //AddForce to rigidbody to move player (speed already defined)
-        rb2d.AddForce(move * speed);
-
-        //Rotation of the Player
-        Vector2 playerpos = Camera.main.WorldToViewportPoint(transform.position);   //Define Player Position
-        Vector2 mousepos = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition); //Define Mouse Position
-        float angle = AngleBetweenTwoPoints(playerpos, mousepos) + 90;   //Solve for the angle between the player and mouse
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));  //Rotate Player
-
-        //Friction: When the player isn't moving, they need to stop moving :(
-        Debug.Log(Input.GetAxis("Horizontal"));
-        */
     }
 
-   
     //Function that finds the angle between two specified vector points
     float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
     {
@@ -205,4 +129,73 @@ public class pl_mvmnt : MonoBehaviour {
        float angle = AngleBetweenTwoPoints(playerpos, mousepos)+90;   //Solve for the angle between the player and mouse
        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));  //Rotate Player
    }
+   */
+
+/*
+ * //Movement of the Player
+if (Input.GetKey(KeyCode.D))
+{
+    Vector3 position = this.transform.position;
+    position.x = position.x + speed;
+    this.transform.position = position;
+}
+if (Input.GetKey(KeyCode.A))
+{
+    Vector3 position = this.transform.position;
+    position.x = position.x - speed;
+    this.transform.position = position;
+}
+if (Input.GetKey(KeyCode.W))
+{
+    Vector3 position = this.transform.position;
+    position.y = position.y + speed;
+    this.transform.position = position;
+}
+if (Input.GetKey(KeyCode.S))
+{
+    Vector3 position = this.transform.position;
+    position.y = position.y - speed;
+    this.transform.position = position;
+}
+*/
+
+/*
+   //Movement with Rigidbodies and Forces
+   //Update speed and curr_stamina by checking for sprinting
+   Sprinting(ref speed, ref curr_stamina, ref stamina_regen);
+
+   //Update Stamina
+   if (curr_stamina < 0)
+   {
+       curr_stamina = 0;
+   }
+   else if (curr_stamina > maxstamina)
+   {
+       curr_stamina = maxstamina;
+   }
+   else
+   {
+       curr_stamina = curr_stamina + stamina_regen;
+   }
+
+   //Get Horizontal Movement
+   float HorizontalMove = Input.GetAxis("Horizontal");
+
+   //Get Vertical Movement
+   float VerticalMove = Input.GetAxis("Vertical");
+
+   //Create movement Vector
+   Vector2 move = new Vector2(HorizontalMove, VerticalMove);
+   //Debug.Log(move);
+   //AddForce to rigidbody to move player (speed already defined)
+   rb2d.AddForce(move * speed);
+
+   //Rotation of the Player
+   Vector2 playerpos = Camera.main.WorldToViewportPoint(transform.position);   //Define Player Position
+   Vector2 mousepos = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition); //Define Mouse Position
+   float angle = AngleBetweenTwoPoints(playerpos, mousepos) + 90;   //Solve for the angle between the player and mouse
+   transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));  //Rotate Player
+
+   //Friction: When the player isn't moving, they need to stop moving :(
+   Debug.Log(Input.GetAxis("Horizontal"));
    */
