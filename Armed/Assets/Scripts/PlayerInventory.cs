ff1b1e3c85cc83ea8a2349b87bc88815abd744cd
Jared Capelle <jjc2bd@virginia.edu>
2018-06-20 21:30:45 -0400

    Made Item Throwing better
    Woot.

--- Armed/Assets/Scripts/PlayerInventory.cs
+++ Armed/Assets/Scripts/PlayerInventory.cs
@@ -22,40 +22,62 @@ public class PlayerInventory : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        ThrowyBoi();
+        ScaleyBoi();
+        ShootyBoi();
+    }
+
+    //Throws Things
+    void ThrowyBoi()
+    {
         if (Input.GetKeyDown(KeyCode.Mouse0) && inventory.Count > 0)
         {
+            GameObject projectile;
             //var inst = new GameObject();
             int a = inventory.Count;
             print("The inventory has stuff in it");
             print("The length of the inventory is " + a);
             print(inventory[a - 1]);
             //There's got to be a better way of doing this... Jared, halp plz
-            if (inventory[a - 1] == "object")
-            {
-                var projectile = (GameObject)Instantiate(objectPrefab, objectSpawn.position, objectSpawn.rotation);
-                inventory.Remove(inventory[a - 1]);
-                projectile.GetComponent<Rigidbody2D>().velocity = projectile.transform.up * 6;   //Add momentum?
-            }
-            else if (inventory[a - 1] == "pistol")
-            {
-                var projectile = (GameObject)Instantiate(pistolPrefab, objectSpawn.position, objectSpawn.rotation);
-                inventory.Remove(inventory[a - 1]);
-                projectile.GetComponent<Rigidbody2D>().velocity = projectile.transform.up * 6;   //Add momentum?
-            }
-            else if (inventory[a - 1] == "shotgun")
-            {
-                var projectile = (GameObject)Instantiate(shotgunPrefab, objectSpawn.position, objectSpawn.rotation);
-                inventory.Remove(inventory[a - 1]);
-                projectile.GetComponent<Rigidbody2D>().velocity = projectile.transform.up * 6;   //Add momentum?
-            }
-            else if (inventory[a - 1] == "machine_gun")
-            {
-                var projectile = (GameObject)Instantiate(machine_gunPrefab, objectSpawn.position, objectSpawn.rotation);
-                inventory.Remove(inventory[a - 1]);
-                projectile.GetComponent<Rigidbody2D>().velocity = projectile.transform.up * 6;   //Add momentum?
-            }
+            //if (inventory[a - 1] == "object")
+            //{
+            //    projectile = Instantiate(objectPrefab, objectSpawn.position, objectSpawn.rotation);
+            //}
+            //else if (inventory[a - 1] == "pistol")
+            //{
+            //    projectile = Instantiate(pistolPrefab, objectSpawn.position, objectSpawn.rotation);
+            //}
+            //else if (inventory[a - 1] == "shotgun")
+            //{
+            //    projectile = Instantiate(shotgunPrefab, objectSpawn.position, objectSpawn.rotation);
+            //}
+            //else if (inventory[a - 1] == "machine_gun")
+            //{
+            //    projectile = Instantiate(machine_gunPrefab, objectSpawn.position, objectSpawn.rotation);
+            //}
+            // https://answers.unity.com/questions/551934/instantiating-using-a-string-for-prefab-name.html
+            projectile = Instantiate(Resources.Load(inventory[a - 1], typeof(GameObject)), objectSpawn.position, objectSpawn.rotation) as GameObject;
+            projectile.GetComponent<Rigidbody2D>().velocity = projectile.transform.up * 6;   //Add momentum?
+            inventory.Remove(inventory[a - 1]);
+
 
         }
+    }
+
+    // Makes Da Guns go Boom
+    //#TODO - Heat-based system Mech-Warrior style for the guns
+    void ShootyBoi()
+    {
+        // #TODO
+    }
+
+    // Scales the player based on the inventory
+    void ScaleyBoi()
+    {
+        // 4 tiers
+        // Add 50 health
+        //#TODO
+        // Also different sized sprites
     }
 
 
