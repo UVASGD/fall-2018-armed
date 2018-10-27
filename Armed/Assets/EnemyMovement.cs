b2fc91780ed49ee09b4ac8fd9671dedeaf68cffb
Omar El Sheikh <techmanozmar@gmail.com>
2018-10-27 14:29:40 -0400

    AI and Gun Manager Stuff
    Added NavePoint and CoverPoint stuff
    Added basic Gun Pickup stuff

---
+++ Armed/Assets/EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public GameObject NavmeshHolder;
+    public Navpoint start;
+    public Navpoint end;
+
+	// Use this for initialization
+	void Start () {
+        int numNavpoints = NavmeshHolder.transform.childCount;
+        Transform[] transforms = NavmeshHolder.GetComponentsInChildren<Transform>();
+        double minDist = Mathf.Infinity;
+        int minIndex = -1;
+        for (int x = 0; x < numNavpoints; x++)
+        {
+            double currDist = Vector3.Distance(transform.position, transforms[x].position);
+            if (currDist < minDist)
+            {
+                minDist = currDist;
+                minIndex = x;
+            }
+        }
+
+        start = transforms[minIndex].GetComponent<Navpoint>();
+        end = transforms[Random.Range(0, numNavpoints)].GetComponent<Navpoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Armed/Assets/GunManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunManager : MonoBehaviour {
+
+    public int maxHeat;
+    public float currHeat;
+    public int numGuns;
+    public float heatDecreaseRatePerGun;
+    public float heatIncreaseRate;
+    public float gunDistAngle;
+    public GameObject gunHolder;
+    private float lastFireTime;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire"))
+        {
+            currHeat = Mathf.Clamp(currHeat - numGuns * heatDecreaseRatePerGun * Time.deltaTime, 0, maxHeat);
+            for (int x = 0; x < numGuns; x++)
+            {
+                //Fire the gun from a specific location
+            }
+        }
+        else
+        {
+            currHeat = Mathf.Clamp(currHeat + heatIncreaseRate * Time.deltaTime, 0, maxHeat);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Collided with: " + other.gameObject.name);
+        if (other.gameObject.tag == "pistol")
+        {
+            numGuns++;
+            other.gameObject.transform.parent = gunHolder.transform;
+            other.gameObject.transform.localPosition = new Vector3(Random.Range(-.5f, .5f), .5f, 0);
+            other.gameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90 + Random.Range(-gunDistAngle / 2, gunDistAngle / 2)));
+        }
+    }
+}
