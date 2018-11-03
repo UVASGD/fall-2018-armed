ece863ede043a43c15242681826eef5ff703794f
Salindar165 <klanax135@gmail.com>
2018-11-03 16:01:52 -0400

    Added Some tilesets
    DRAWING

---
+++ Armed/Assets/Art/Tilesets/Env_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Env_1" tilewidth="64" tileheight="64" tilecount="64" columns="4">
+ <image source="Source Images/Env_1.png" width="256" height="256"/>
+</tileset>
---
+++ Armed/Assets/Art/Tilesets/Floors_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Floors_2" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="Source Images/Floors_2.png" width="512" height="512"/>
+</tileset>
