daf77ef8906dd3e111eefcc723790778ab484d82
Alex Kim <aak3cf@virginia.edu>
2018-11-09 21:11:02 -0500

    Made the First level + added tiled2Unity

--- Armed/Assets/Art/Tilesets/Env_1.tsx
+++ Armed/Assets/Art/Tilesets/Env_1.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Env_1" tilewidth="64" tileheight="64" tilecount="64" columns="4">
+<tileset version="1.2" tiledversion="1.2.0" name="Env_1" tilewidth="64" tileheight="64" tilecount="16" columns="4">
  <image source="Source Images/Env_1.png" width="256" height="256"/>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="4" y="4" width="59" height="54"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="4" width="59" height="54"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="0" width="62" height="63"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="63" height="63"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="63" height="63"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="63" height="63"/>
+  </objectgroup>
+ </tile>
 </tileset>
--- Armed/Assets/Art/Tilesets/Walls.tsx
+++ Armed/Assets/Art/Tilesets/Walls.tsx
@@ -1,17 +1,29 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Walls" tilewidth="64" tileheight="64" tilecount="5" columns="0">
+<tileset version="1.2" tiledversion="1.2.0" name="Walls" tilewidth="64" tileheight="64" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1">
   <image width="64" height="64" source="../Environment/Walls/Thin Wall vertical.png"/>
+  <objectgroup draworder="index">
+   <object id="5" x="41" y="0" width="22" height="63"/>
+  </objectgroup>
  </tile>
  <tile id="2">
   <image width="64" height="64" source="../Environment/Walls/Thin Wall vertical_2.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="2" y="1" width="20" height="62"/>
+  </objectgroup>
  </tile>
  <tile id="3">
   <image width="64" height="64" source="../Environment/Walls/Thin Wall.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="1" width="63" height="21"/>
+  </objectgroup>
  </tile>
  <tile id="4">
   <image width="64" height="64" source="../Environment/Walls/Thin Wall horiz.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="42" width="63" height="19"/>
+  </objectgroup>
  </tile>
  <tile id="5">
   <image width="64" height="64" source="../Environment/Walls/Thin Wall Corner.png"/>
