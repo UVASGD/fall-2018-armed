6d1a4fd1bc51c62064d2d4640b8f748872590ffc
Salindar165 <klanax135@gmail.com>
2018-11-09 19:32:20 -0500

    WALLLZ
    Walls are here

---
+++ Armed/Assets/Art/Tilesets/Walls.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Walls" tilewidth="64" tileheight="64" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="64" height="64" source="../Environment/Walls/Thin Wall vertical.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../Environment/Walls/Thin Wall vertical_2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../Environment/Walls/Thin Wall.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../Environment/Walls/Thin Wall horiz.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../Environment/Walls/Thin Wall Corner.png"/>
+ </tile>
+</tileset>
