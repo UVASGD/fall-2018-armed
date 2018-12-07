8e2a2ca1a8435d3d24b6188a9da573946326307e
Salindar165 <klanax135@gmail.com>
2018-12-07 18:44:28 -0500

    Added an End Level Script
    You can transition Levels now

---
+++ Armed/Assets/Scripts/load_scene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class load_scene : MonoBehaviour {
+    public string load_level;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine("level_change");
+        Debug.Log("I'm colliding");
+    }
+
+    IEnumerator level_change()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(load_level); //Load Level
+        //Wait until fully loaded
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
