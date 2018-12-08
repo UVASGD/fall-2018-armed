2b13ff4c978b0581f62395ae840bdbd740b7085e
Salindar165 <klanax135@gmail.com>
2018-12-07 19:13:48 -0500

    *SCREAM*
    I dunno, man

---
+++ Armed/Assets/Scripts/Menu/Quit Button.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour {
+    void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("it do");
+    }
+}
