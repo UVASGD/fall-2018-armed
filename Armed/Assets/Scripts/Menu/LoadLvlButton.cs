8aaca752694679c277ebbe3c095634c5a67308dc
Salindar165 <klanax135@gmail.com>
2018-12-09 01:08:45 -0500

    Credits
    Plz edit

---
+++ Armed/Assets/Scripts/Menu/LoadLvlButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadLvlButton : MonoBehaviour
+{
+    public Button button;
+    public int level;
+    void Start()
+    {
+        //Calls the TaskOnClick/TaskWithParameters/ButtonClicked method when you click the Button
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        StartCoroutine("level_change");
+    }
+    IEnumerator level_change()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(level); //Load Level
+        //Wait until fully loaded
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
